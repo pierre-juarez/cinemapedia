94d0b95b398cb0aab273f4649888843342b3371a
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2024-05-14 00:44:49 -0500

    [add] - Scroll infinito

--- lib/infraestructure/datasources/moviedb_datasource.dart
+++ lib/infraestructure/datasources/moviedb_datasource.dart
@@ -17,7 +17,7 @@ class MoviedbDatasource extends MoviesDataSource {
   );
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response = await dio.get('/movie/now_playing');
+    final response = await dio.get('/movie/now_playing', queryParameters: {'page': page});
     final movieDBResponse = MovieDbResponse.fromJson(response.data);
     final List<Movie> movies = movieDBResponse.results
         .where((movie) => movie.posterPath != 'no-poster')
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -44,6 +44,9 @@ class _HomeViewState extends ConsumerState<_HomeView> {
           movies: nowPlayingMovies,
           title: 'En cines',
           subTitle: 'Lunes 20',
+          loadNextPage: () {
+            ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+          },
         )
         // Expanded(
         //   child: ListView.builder(
