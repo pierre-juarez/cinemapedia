e10479706f2bb330979e6cf87f19f82a6fe8fdc5
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2024-07-05 23:39:44 -0500

    [add] - Search movies

--- lib/infraestructure/datasources/moviedb_datasource.dart
+++ lib/infraestructure/datasources/moviedb_datasource.dart
@@ -58,4 +58,13 @@ class MoviedbDatasource extends MoviesDataSource {
     final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response = await dio.get(
+      '/search/movie',
+      queryParameters: {'query': query},
+    );
+    return _jsonToMovies(response.data);
+  }
 }
