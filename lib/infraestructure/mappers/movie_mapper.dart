6767d71ceb03e19d1a97d4706995a44d55883c8a
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2024-07-24 00:41:32 -0500

    [add] - Debounce manual, search dynamic

--- lib/infraestructure/mappers/movie_mapper.dart
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -14,8 +14,10 @@ class MovieMapper {
       originalTitle: moviedb.originalTitle,
       overview: moviedb.overview,
       popularity: moviedb.popularity,
-      posterPath: moviedb.posterPath != '' ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}' : 'no-poster',
-      releaseDate: moviedb.releaseDate,
+      posterPath: moviedb.posterPath != ''
+          ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
+          : 'https://static.displate.com/280x392/displate/2023-11-05/14fc8d14565a3a2134f4ad62c0ef212d_52bca31461a67d5b987a3a06224b8c0b.jpg',
+      releaseDate: moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
       title: moviedb.title,
       video: moviedb.video,
       voteAverage: moviedb.voteAverage,
