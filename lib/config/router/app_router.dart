383f90d62a04e7142183d38d315d111a9152b4aa
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2024-09-07 00:29:19 -0500

    [add] - Go Router - Tabs

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,15 +1,28 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(path: '/', name: HomeScreen.name, builder: (context, state) => const HomeScreen(), routes: [
-    GoRoute(
-      path: 'movie/:id',
-      name: MovieScreen.name,
+final appRouter = GoRouter(initialLocation: '/home/0', routes: [
+  GoRoute(
+      path: '/home/:page',
+      name: HomeScreen.name,
       builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? "no-id";
-        return MovieScreen(movieId: movieId);
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+
+        return HomeScreen(pageIndex: pageIndex);
       },
-    )
-  ]),
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+
+            return MovieScreen(movieId: movieId);
+          },
+        ),
+      ]),
+  GoRoute(
+    path: '/',
+    redirect: (_, __) => '/home/0',
+  ),
 ]);
